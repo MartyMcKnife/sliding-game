965e2d0229e9d8e45e7ffe02f2fe81b37586b59d
Sean McDougall <mcdougalls@wesley.wa.edu.au>
2022-06-27 18:17:07 +0800

    Added finding positions

--- __tests__/utils/logic.test.ts
+++ __tests__/utils/logic.test.ts
@@ -1,31 +1,100 @@
-import { swap } from './../../utils/logic';
+import { swap, findItem, findNearby } from "./../../utils/logic";
 
-describe('Swap Function', () => {
-    const testData = {
-        array: [[0,1,2],[3,4,5],[6,7,8]],
-        position1: {
-            row: 0,
-            column: 0
+const testData = {
+  array: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ],
+  position1: {
+    row: 0,
+    column: 0,
+  },
+  position2: {
+    row: 1,
+    column: 0,
+  },
+  item: 1,
+};
+
+describe("Swap Function", () => {
+  test("should return a multidimensional array", () => {
+    const output = swap(testData.array, testData.position1, testData.position2);
+    expect(Array.isArray(output)).toBe(true);
+    expect(Array.isArray(output[0])).toBe(true);
+  });
+  test("should swap values", () => {
+    const output = swap(testData.array, testData.position1, testData.position2);
+    expect(output).toStrictEqual([
+      [3, 1, 2],
+      [0, 4, 5],
+      [6, 7, 8],
+    ]);
+  });
+  test("should return same array if out of bounds", () => {
+    const output = swap(testData.array, testData.position1, {
+      row: 9,
+      column: 1,
+    });
+    expect(output).toStrictEqual(testData.array);
+  });
+});
+
+describe("Find item", () => {
+  test("Returns object with row and column", () => {
+    const output = findItem(testData.array, testData.item);
+
+    expect(output).toHaveProperty("row");
+    expect(output).toHaveProperty("column");
+  });
+
+  test("Returns object with right row and column", () => {
+    const output = findItem(testData.array, testData.item);
+
+    expect(output).toHaveProperty("row", 0);
+    expect(output).toHaveProperty("column", 1);
+  });
+
+  test("Returns null if no item found", () => {
+    expect(findItem(testData.array, 99)).toBeNull();
+  });
+});
+
+describe("Find Nearby", () => {
+  const position = findItem(testData.array, testData.item);
+  test("should return four items", () => {
+    expect(findNearby(testData.array, position)).toMatchObject({
+      top: null,
+      bottom: expect.anything(),
+      left: expect.anything(),
+      right: expect.anything(),
+    });
+  });
+
+  test("should return the right numbers", () => {
+    expect(findNearby(testData.array, position)).toMatchObject({
+      top: null,
+      left: {
+        item: 0,
+        position: {
+          row: 0,
+          column: 0,
         },
-        position2: {
-            row: 1,
-            column: 0
-        }
-
-    } 
-    test('should return a multidimensional array', () => { 
-        const output = swap(testData.array, testData.position1, testData.position2)
-        expect(Array.isArray(output)).toBe(true)
-        expect(Array.isArray(output[0])).toBe(true)
-     })
-     test('should swap values', () => {
-        console.log(`Before swap: ${testData.array}`)
-        const output = swap(testData.array, testData.position1, testData.position2)
-        console.log(`After swap: ${output} (Original: ${testData.array})`)
-        expect(output).toStrictEqual([[3,1,2],[0,4,5],[6,7,8]])
-     } )
-     test('should return same array if out of bounds', () => {
-        const output = swap(testData.array, testData.position1, {row: 9, column: 1})
-        expect(output).toStrictEqual(testData.array)
-     })
- })
+      },
+      right: {
+        item: 2,
+        position: {
+          row: 0,
+          column: 2,
+        },
+      },
+      bottom: {
+        item: 4,
+        position: {
+          row: 1,
+          column: 1,
+        },
+      },
+    });
+  });
+});
--- utils/logic.ts
+++ utils/logic.ts
@@ -1,21 +1,73 @@
 interface Position {
-    row: number;
-    column: number;
+  row: number;
+  column: number;
 }
 
+export const swap = (
+  array: Array<Array<number>>,
+  position1: Position,
+  position2: Position
+) => {
+  //Copy array so we don't mutate the original
+  //We have to use JSON.parse and stringify because of JS weirdness
+  const arr = JSON.parse(JSON.stringify(array));
+  try {
+    const temp = arr[position1.row][position1.column];
+    arr[position1.row][position1.column] = arr[position2.row][position2.column];
+    arr[position2.row][position2.column] = temp;
+  } catch (error) {
+    console.error(error);
+  }
 
-export const swap = (array: Array<Array<number>>, position1: Position, position2: Position) => {
-    //Copy array so we don't mutate the original
-    //We have to use JSON.parse and stringify because of JS weirdness
-    const arr = JSON.parse(JSON.stringify(array));
-    try {
-        const temp = arr[position1.row][position1.column];
-        arr[position1.row][position1.column] = arr[position2.row][position2.column];
-        arr[position2.row][position2.column] = temp
+  return arr;
+};
 
-    } catch (error) {
-        console.error(error)
-    }
+export const findItem = (
+  array: Array<Array<number>>,
+  element: number | string
+) => {
+  let position: Position;
 
-    return arr
-}
+  array.map((row, iR) => {
+    row.map((column, iC) => {
+      if (column === element) {
+        position = { row: iR, column: iC };
+      }
+    });
+  });
+  //@ts-ignore
+  return position || null;
+};
+
+export const findNearby = (array: Array<Array<number>>, curItem: Position) => {
+  const left =
+    curItem.column > 0
+      ? {
+          item: array[curItem.row][curItem.column - 1],
+          position: { row: curItem.row, column: curItem.column - 1 },
+        }
+      : null;
+  const right =
+    curItem.column < array[0].length
+      ? {
+          item: array[curItem.row][curItem.column + 1],
+          position: { row: curItem.row, column: curItem.column + 1 },
+        }
+      : null;
+  const top =
+    curItem.row > 0
+      ? {
+          item: array[curItem.row - 1][curItem.column],
+          position: { row: curItem.row - 1, column: curItem.column },
+        }
+      : null;
+  const bottom =
+    curItem.row < array.length
+      ? {
+          item: array[curItem.row + 1][curItem.column],
+          position: { row: curItem.row + 1, column: curItem.column },
+        }
+      : null;
+
+  return { left, right, top, bottom };
+};
